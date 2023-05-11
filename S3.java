0a62ca523f84f2c72908d70f9801c8dca71d714c
shreyass0719 <133000603+shreyass0719@users.noreply.github.com>
2023-05-11 14:59:02 +0530

    Create S3.java

---
+++ S3.java
@@ -0,0 +1,11 @@
+public class S3{
+  public static void main(String[]arg){
+    int a=78;
+    int b=58;
+    int c=98;
+    ans1=a-b;
+    ans2=a+b;
+    ans3=a/b;
+    System.out.println("ans is"+ans1);
+    System.out.println("ans is"+ans2);
+    System.out.println("ans is"+ans3);
