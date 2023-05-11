168107404070cf2660ffbaa4d874e0319a81202c
shreyass0719 <133000603+shreyass0719@users.noreply.github.com>
2023-05-11 15:52:13 +0530

    Update S3.java

--- S3.java
+++ S3.java
@@ -2,10 +2,9 @@ public class S3{
   public static void main(String[]arg){
     int a=78;
     int b=58;
-    int c=98;
-    ans1=a-b;
-    ans2=a+b;
-    ans3=a/b;
+    int ans1=a-b;
+    int ans2=a+b;
+    double ans3=a/b;
     System.out.println("ans is"+ans1);
     System.out.println("ans is"+ans2);
     System.out.println("ans is"+ans3);
